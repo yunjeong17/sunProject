34ae4725396d4f2054ca177d9753d332464df811
yunjeong17 <var0701@naver.com>
2021-08-10 13:40:11 +0900

    dml update

---
+++ script/순대학교 DML 강의.sql
@@ -0,0 +1,48 @@
+-- 강의 INSERT
+
+--INSERT INTO PROFESSORS
+--VALUES('P50101','lo','로재석','C501','010-1021-1021','lojae@sun.co.kr'); --기계공학
+--INSERT INTO PROFESSORS
+--VALUES('P50201','lu','류재석','C502','010-1161-1161','lujae@sun.co.kr'); --항공우주공학
+--INSERT INTO PROFESSORS
+--VALUES('P50301','chu','츄재석','C503','010-1114-1114','chujae@sun.co.kr'); --전자전기공학
+--INSERT INTO PROFESSORS
+--VALUES('P50401','wj','왕재석','C504','010-1414-1414','wjae@sun.co.kr'); --컴퓨터공학
+--INSERT INTO PROFESSORS
+--VALUES('P50501','wjs','왕쥬석','C505','010-1313-1313','wangjae@sun.co.kr'); --오징어심리학과
+--INSERT INTO PROFESSORS
+--VALUES('P50601','mouse','왕쥐석','C506','010-1717-1717','wangmouse@sun.co.kr'); --건축학과
+--INSERT INTO PROFESSORS
+--VALUES('P50701','jee','강지석','C507','010-7575-7575','kangjee@sun.co.kr'); --산업공학과
+--INSERT INTO PROFESSORS
+--VALUES('P50801','loba','로바트','C508','010-4447-4445','loba@sun.co.kr'); --원자핵공학과
+--INSERT INTO PROFESSORS
+--VALUES('P50901','josun','헬조선','C509','010-1231-1756','josun@sun.co.kr'); --조선해양공학과
+--INSERT INTO PROFESSORS
+--VALUES('P51001','energy','이자원','C510','010-6675-3534','energy@sun.co.kr'); --에너지자원공학과
+--INSERT INTO PROFESSORS
+--VALUES('P51101','embedded','이임배','C511','010-2312-4632','embedded@sun.co.kr'); --임베디드시스템공학과
+
+
+INSERT INTO CLASS VALUES('L000','소프트웨어공학','IT관 204호',2020,1,1,'P50401');
+INSERT INTO CLASS VALUES('L001','디지털공학설계','IT관 301호',2020,1,1,'P50401');
+INSERT INTO CLASS VALUES('L002','알고리즘의 이해','IT관 503호',2020,1,1,'P50401');
+INSERT INTO CLASS VALUES('L003','C언어 프로그래밍','IT관 112호',2020,1,1,'P50401');
+
+INSERT INTO CLASS VALUES('L004','객체지향언어','IT관 508호',2020,1,1,'P50401');
+INSERT INTO CLASS VALUES('L005','공학수학I','정보관 204호',2020,1,1,'P50401');
+INSERT INTO CLASS VALUES('L006','공학수학II','정보관 301호',2020,2,1,'P50401');
+INSERT INTO CLASS VALUES('L007','열역학','정보관 503호',2020,2,2,'P50401');
+INSERT INTO CLASS VALUES('L008','현대소설론','중앙도서관 112호',2020,2,1,'P50401');
+INSERT INTO CLASS VALUES('L009','국어음운론','중앙도서관 508호',2020,2,1,'P50401');
+
+INSERT INTO CLASS VALUES('L010','비교문학론','인문사회관 204호',2020,2,1,'P50401');
+INSERT INTO CLASS VALUES('L011','디지털공학설계','인문사회관 301호',2020,2,1,'P50401');
+INSERT INTO CLASS VALUES('L012','알고리즘의 이해','인문사회관 503호',2020,2,1,'P50401');
+INSERT INTO CLASS VALUES('L013','C언어 프로그래밍','종합강의관 112호',2020,2,1,'P50401');
+INSERT INTO CLASS VALUES('L014','객체지향언어','종합강의관 508호',2020,2,1,'P50401');
+INSERT INTO CLASS VALUES('L005','국문학개론','정보관 204호',2020,1,1,'P50401');
+INSERT INTO CLASS VALUES('L006','심리학개론','정보관 301호',2020,2,1,'P50401');
+INSERT INTO CLASS VALUES('L007','선형대수학','정보관 503호',2020,2,2,'P50401');
+INSERT INTO CLASS VALUES('L008','현대소설론','중앙도서관 112호',2020,2,1,'P50401');
+INSERT INTO CLASS VALUES('L009','국어음운론','중앙도서관 508호',2020,2,1,'P50401');
