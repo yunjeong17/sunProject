52b13aa296a4029a4b9155f11138e9d7af4703a2
yunjeong17 <var0701@naver.com>
2021-08-10 12:26:00 +0900

    data type update

--- script/순대학교 DML 교원.sql
+++ script/순대학교 DML 교원.sql
@@ -1,118 +1,117 @@
 --인문
 INSERT INTO PROFESSORS
-VALUES('P10101','kor','왕대한','D100','C101','010-9090-9090','korean@sun.co.kr');
+VALUES('P10101','kor','왕대한','C101','010-9090-9090','korean@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P10201','man','왕중국','D100','C102','010-8888-8888','mandarin@sun.co.kr');
+VALUES('P10201','man','왕중국','C102','010-8888-8888','mandarin@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P10301','eng','왕영어','D100','C103','010-0000-0000','english@sun.co.kr');
+VALUES('P10301','eng','왕영어','C103','010-0000-0000','english@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P10401','fra','왕불어','D100','C104','010-3333-3333','french@sun.co.kr');
+VALUES('P10401','fra','왕불어','C104','010-3333-3333','french@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P10501','korhis','왕역사','D100','C105','010-0404-0404','khistory@sun.co.kr');
+VALUES('P10501','korhis','왕역사','C105','010-0404-0404','khistory@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P10601','verb','왕동양','D100','C106','010-5050-5050','oriental@sun.co.kr');
+VALUES('P10601','verb','왕동양','C106','010-5050-5050','oriental@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P10701','western','왕서양','D100','C107','010-4444-4444','western@sun.co.kr');
+VALUES('P10701','western','왕서양','C107','010-4444-4444','western@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P10801','phd','소크라테스','D100','C108','010-3030-3030','philosophy@sun.co.kr');
+VALUES('P10801','phd','소크라테스','C108','010-3030-3030','philosophy@sun.co.kr');
 
 --사회과학
 INSERT INTO PROFESSORS
-VALUES('P20101','politics','김무성','D200','C201','010-5478-2183','politics@sun.co.kr');
+VALUES('P20101','politics','김무성','C201','010-5478-2183','politics@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P20201','society','김엔프피','D200','C202','010-8282-8282','society@sun.co.kr');
+VALUES('P20201','society','김엔프피','C202','010-8282-8282','society@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P20301','sapience','김사피엔스','D200','C203','010-0718-8710','sapience@sun.co.kr');
+VALUES('P20301','sapience','김사피엔스','C203','010-0718-8710','sapience@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P20401','psychology','김심령','D200','C204','010-1231-2534','psychology@sun.co.kr');
+VALUES('P20401','psychology','김심령','C204','010-1231-2534','psychology@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P20501','geology','김지오','D200','C205','010-5756-7685','geology@sun.co.kr');
+VALUES('P20501','geology','김지오','C205','010-5756-7685','geology@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P20601','hatepeople','김인간혐오','D200','C206','010-4444-4444','hatepeople@sun.co.kr');
+VALUES('P20601','hatepeople','김인간혐오','C206','010-4444-4444','hatepeople@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P20701','press','정경유착','D200','C207','010-2342-4564','press@sun.co.kr');
+VALUES('P20701','press','정경유착','C207','010-2342-4564','press@sun.co.kr');
 
 --자연과학
 INSERT INTO PROFESSORS
-VALUES('P30101','suri','마수리','D300','C301','010-3737-7465','suri@sun.co.kr');
+VALUES('P30101','suri','마수리','C301','010-3737-7465','suri@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P30201','tong','상집게','D300','C302','010-5740-0912','suri@sun.co.kr');
+VALUES('P30201','tong','상집게','C302','010-5740-0912','suri@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P30301','chem','상화학','D300','C303','010-4040-5000','chem@sun.co.kr');
+VALUES('P30301','chem','상화학','C303','010-4040-5000','chem@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P30401','water','상물리','D300','C304','010-0202-3023','water@sun.co.kr');
+VALUES('P30401','water','상물리','C304','010-0202-3023','water@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P30501','biology','상생명','D300','C305','010-8879-7988','biology@sun.co.kr');
+VALUES('P30501','biology','상생명','C305','010-8879-7988','biology@sun.co.kr');
 
 --경영대학
 INSERT INTO PROFESSORS
-VALUES('P40101','run','왕수빈','D400','C401','010-3371-5190','ksubin0915@sun.co.kr');
+VALUES('P40101','run','왕수빈','C401','010-3371-5190','ksubin0915@sun.co.kr');
 
 --공과대학
 INSERT INTO PROFESSORS
-VALUES('P50101','lo','로재석','D500','C501','010-1021-1021','lojae@sun.co.kr');
+VALUES('P50101','lo','로재석','C501','010-1021-1021','lojae@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P50201','lu','류재석','D500','C502','010-1161-1161','lujae@sun.co.kr');
+VALUES('P50201','lu','류재석','C502','010-1161-1161','lujae@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P50301','chu','츄재석','D500','C503','010-1114-1114','chujae@sun.co.kr');
+VALUES('P50301','chu','츄재석','C503','010-1114-1114','chujae@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P50401','wj','왕재석','D500','C504','010-1414-1414','wjae@sun.co.kr');
+VALUES('P50401','wj','왕재석','C504','010-1414-1414','wjae@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P50501','wjs','왕쥬석','D500','C505','010-1313-1313','wangjae@sun.co.kr');
+VALUES('P50501','wjs','왕쥬석','C505','010-1313-1313','wangjae@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P50601','mouse','왕쥐석','D500','C506','010-1717-1717','wangmouse@sun.co.kr');
+VALUES('P50601','mouse','왕쥐석','C506','010-1717-1717','wangmouse@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P50701','jee','강지석','D500','C507','010-7575-7575','kangjee@sun.co.kr');
+VALUES('P50701','jee','강지석','C507','010-7575-7575','kangjee@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P50801','loba','로바트','D500','C508','010-4447-4445','loba@sun.co.kr');
-
+VALUES('P50801','loba','로바트','C508','010-4447-4445','loba@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P50901','josun','헬조선','D500','C509','010-1231-1756','josun@sun.co.kr');
+VALUES('P50901','josun','헬조선','C509','010-1231-1756','josun@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P51001','energy','이자원','D500','C510','010-6675-3534','energy@sun.co.kr');
+VALUES('P51001','energy','이자원','C510','010-6675-3534','energy@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P51101','embedded','이임배','D500','C511','010-2312-4632','embedded@sun.co.kr');
+VALUES('P51101','embedded','이임배','C511','010-2312-4632','embedded@sun.co.kr');
 
 --농업생명과학대학
-INSERT INTO CATEGORY VALUES('P60101','plant','식무럭','D600','C601','011-4578-8451','plant@sun.co.kr');
-INSERT INTO CATEGORY VALUES('P60201','forest','산녹림','D600','C602','018-4001-4578','forest@sun.co.kr');
-INSERT INTO CATEGORY VALUES('P60301','appliy','임혈구','D600','C603','017-0625-9531','appliy@sun.co.kr');
-INSERT INTO CATEGORY VALUES('P60401','mandarin','최주도','D600','C604','001-3265-6481','mandarin@sun.co.kr');
-INSERT INTO CATEGORY VALUES('P60501','poison','독심술','D600','C605','070-5218-9547','poison@sun.co.kr');
-INSERT INTO CATEGORY VALUES('P60601','bio','최민철','D600','C606','089-1524-4440','bio@sun.co.kr');
-INSERT INTO CATEGORY VALUES('P60701','agric','심수련','D600','C607','099-1623-5555','agric@sun.co.kr');
-INSERT INTO CATEGORY VALUES('P60801','land','소나무','D600','C608','060-1577-1577','land@sun.co.kr');
+INSERT INTO PROFESSORS VALUES('P60101','plant','식무럭','C601','011-4578-8451','plant@sun.co.kr');
+INSERT INTO PROFESSORS VALUES('P60201','forest','산녹림','C602','018-4001-4578','forest@sun.co.kr');
+INSERT INTO PROFESSORS VALUES('P60301','appliy','임혈구','C603','017-0625-9531','appliy@sun.co.kr');
+INSERT INTO PROFESSORS VALUES('P60401','mandarin','최주도','C604','001-3265-6481','mandarin@sun.co.kr');
+INSERT INTO PROFESSORS VALUES('P60501','poison','독심술','C605','070-5218-9547','poison@sun.co.kr');
+INSERT INTO PROFESSORS VALUES('P60601','bio','최민철','C606','089-1524-4440','bio@sun.co.kr');
+INSERT INTO PROFESSORS VALUES('P60701','agric','심수련''C607','099-1623-5555','agric@sun.co.kr');
+INSERT INTO PROFESSORS VALUES('P60801','land','소나무','C608','060-1577-1577','land@sun.co.kr');
 
 --음악대학
 INSERT INTO PROFESSORS
-VALUES('P70101','vocal','김수미','D700','C701','010-8876-5211','vocal@sun.co.kr');
+VALUES('P70101','vocal','김수미','C701','010-8876-5211','vocal@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P70201','composit','돈스파이크','D700','C702','010-1230-9832','composition@sun.co.kr');
+VALUES('P70201','composit','돈스파이크','C702','010-1230-9832','composition@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P70301','instrumental','김소울','D700','C703','010-9354-9085','instrumental@sun.co.kr');
+VALUES('P70301','instrumental','김소울','C703','010-9354-9085','instrumental@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P70401','folk','강민요','D700','C704','010-5112-6732','folk@sun.co.kr');
+VALUES('P70401','folk','강민요','C704','010-5112-6732','folk@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P70501','ktm','박명장','D700','C705','010-7156-6799','koreanmusic@sun.co.kr');
+VALUES('P70501','ktm','박명장','C705','010-7156-6799','koreanmusic@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P70601','whistle','김휘파람','D700','C706','010-3422-3432','whistle@sun.co.kr');
+VALUES('P70601','whistle','김휘파람','C706','010-3422-3432','whistle@sun.co.kr');
 
 
 --생활과학대학
 INSERT INTO PROFESSORS
-VALUES('P80101','cc','김소비','D800','C801','010-6643-5461','consumer@sun.co.kr');
+VALUES('P80101','cc','김소비','C801','010-6643-5461','consumer@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P80201','fan','박영양','D800','C802','010-9842-9025','food@sun.co.kr');
+VALUES('P80201','fan','박영양','C802','010-9842-9025','food@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P80301','fashion','김패피','D800','C803','010-1252-3562','fashion@sun.co.kr');
+VALUES('P80301','fashion','김패피','C803','010-1252-3562','fashion@sun.co.kr');
 
 
 --의과대학
 INSERT INTO PROFESSORS
-VALUES('P90101','med','김의사','D900','C901','010-2334-5461','medicine@sun.co.kr');
+VALUES('P90101','med','김의사','C901','010-2334-5461','medicine@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P90201','vet','최박사','D900','C902','010-5556-3525','veterinary@sun.co.kr');
+VALUES('P90201','vet','최박사','C902','010-5556-3525','veterinary@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P90301','pha','진박사','D900','C903','010-7132-3332','pharmacy@sun.co.kr');
+VALUES('P90301','pha','진박사','C903','010-7132-3332','pharmacy@sun.co.kr');
 INSERT INTO PROFESSORS
-VALUES('P90401','organs','이박사','D900','C904','010-6633-8016','organs@sun.co.kr');
+VALUES('P90401','organs','이박사','C904','010-6633-8016','organs@sun.co.kr');
