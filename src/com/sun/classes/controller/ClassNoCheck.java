f404fa4e15ca797b3450086a73deaabf7eb989ef
sungdaekimz <84118521+sungdaekimz@users.noreply.github.com>
2021-08-29 14:20:59 +0900

    Add files via upload

---
+++ src/com/sun/classes/controller/ClassNoCheck.java
@@ -0,0 +1,53 @@
+package com.sun.classes.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.classes.model.service.ClassService;
+import com.sun.professors.model.service.ProfessorsService;
+
+/**
+ * Servlet implementation class ClassNoCheck
+ */
+@WebServlet("/noCheck.class")
+public class ClassNoCheck extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ClassNoCheck() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String classNo = request.getParameter("classNo");
+		int result = new ClassService().classNoCheck(classNo);
+		
+		PrintWriter out = response.getWriter();
+		if(result>0) {
+			out.print("fail");
+		}else {
+			out.print("success");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
